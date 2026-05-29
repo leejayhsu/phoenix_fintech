757697ffe45ceb94c2aefa96052a8f5842203817
Leejay Hsu <37034741+leejayhsu@users.noreply.github.com>
2026-05-29 16:39:54 -0700

    feat: configurable force_ssl

--- config/prod.exs
+++ config/prod.exs
@@ -8,17 +8,16 @@ import Config
 config :phoenix_fintech, PhoenixFintechWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
-# Force using SSL in production. This also sets the "strict-security-transport" header,
-# known as HSTS. If you have a health check endpoint, you may want to exclude it below.
-# Note `:force_ssl` is required to be set at compile-time.
-config :phoenix_fintech, PhoenixFintechWeb.Endpoint,
-  force_ssl: [
-    rewrite_on: [:x_forwarded_proto],
-    exclude: [
-      # paths: ["/health"],
-      hosts: ["localhost", "127.0.0.1"]
+if System.get_env("FORCE_SSL") in ~w(true 1) do
+  config :phoenix_fintech, PhoenixFintechWeb.Endpoint,
+    force_ssl: [
+      rewrite_on: [:x_forwarded_proto],
+      exclude: [
+        # paths: ["/health"],
+        hosts: ["localhost", "127.0.0.1"]
+      ]
     ]
-  ]
+end
 
 # Configure Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Req
